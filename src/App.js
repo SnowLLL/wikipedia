af0dbbabcb212e9d412713befb784b9800f76ed6
wiki <liuxueworking@gmail.com>
2021-03-24 23:34:38 -0700

    Delete react-web elements

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Click here for a random article</p>
+      <form>
+
+      </form>
+      <p>Click icon to search</p>
+    </div>
+  );
+}
+
+export default App;
