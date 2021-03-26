e9f3d63f0d3e3c01a113c858c14ebbb086e7e9d5
wiki <liuxueworking@gmail.com>
2021-03-25 23:07:07 -0700

    create elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+import './index.css'
+
+const displayHandler = () =>{
+  document.getElementById('searchIcon').style.visibility = 'hidden';
+  document.getElementById('inputBar').style.visibility = 'visible';
+}
+const reset = () => {
+  document.getElementById('searchIcon').style.visibility = 'visible';
+  document.getElementById('inputBar').style.visibility = 'hidden';
+}
 
 function App() {
   return (
     <div className="App">
-      <p>Click here for a random article</p>
       <div>
-         <i class="fas fa-search fa-7x"></i>
+         <p>Click here for a random article</p>
+         <div id="searchIcon">
+            <i class="fas fa-search fa-7x" onClick ={displayHandler}></i>
+         </div>
+         <div id="inputBar">
+            <input type="text" id="searchWords"></input>
+            <i class="fas fa-times fa-3x" id="resetBIcon" onClick={reset}></i>
+         </div>
+         <p id="description">Click icon to search</p>
       </div>
-      <p>Click icon to search</p>
     </div>
   );
 }
