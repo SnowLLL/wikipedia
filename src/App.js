ec15ec86da4c0b2f00f94e8085efba862b54af21
wiki <liuxueworking@gmail.com>
2021-03-27 18:15:21 -0700

    add rotatation of img

--- src/App.js
+++ src/App.js
@@ -5,25 +5,18 @@ function App() {
   const [state, setstate] = React.useState({
     value:''
   })
-  const [items, setItems] = React.useState({
-    title:'',
-    pageid:''
-  });
+  // const [items, setItems] = React.useState({
+  //   title:'',
+  //   pageid:''
+  // });
 
   const displayHandler = (e) =>{
     document.getElementById('searchIcon').style.visibility = 'hidden';
     document.getElementById('inputBar').style.visibility = 'visible';
-    document.getElementById('description').style.visibility = 'hidden';
-    
   }
   const reset = () => {
     document.getElementById('searchIcon').style.visibility = 'visible';
     document.getElementById('inputBar').style.visibility = 'hidden';
-    document.getElementById('description').style.visibility = 'visible';
-    document.getElementById('searchResult').innerHTML ='';
-    setstate({
-      value:''
-    });
   }
 
   const handleChange = (e) => {
@@ -33,7 +26,6 @@ function App() {
   }
   const handleSubmit = (e) => {
     e.preventDefault();
-    document.getElementById('inputBar').style.visibility = 'visible';
     getContents();
   }
 
@@ -41,15 +33,9 @@ function App() {
     fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${state.value}`)
     .then(res=>res.json())
     .then(result=>{
-      result = result.query.search;
-
-      result.map((item)=>{
-        setItems({
-          title: item.title,
-          pageid: item.pageid
-        })
-        return document.getElementById('searchResult').insertAdjacentHTML('beforeend',
-               "<div><a href='https://en.wikipedia.org/?curid="+item.pageid+"'><h3>"+item.title+"</h3></a></div>");
+      result.query.search.map((item)=>{
+        document.getElementById('searchResult').insertAdjacentHTML('afterend',
+              "<p><a href='https://en.wikipedia.org/?curid="+item.pageid+"'><h3>"+item.title+"</h3></a></p>");
       })
     })
   }
@@ -57,18 +43,18 @@ function App() {
   return (
     <div className="App">
       <div>
-         <p>Click here for a random article</p>
+         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/657px-Wikipedia-logo-v2.svg.png" alt="Wikipedia Logo" />
          <div id="searchIcon">
-            <i className="fas fa-search fa-7x" onClick ={displayHandler}></i>
+            <i className="fas fa-search fa-5x" onClick ={displayHandler}></i>
+            <p>Click icon to search</p>
          </div>
          <div id="inputBar"> 
             <form onSubmit={handleSubmit}>
               <input type="text" id="searchWords" name='words' value={state.value} onChange={handleChange}></input>
               <i className="fas fa-times fa-3x" id="resetBIcon" onClick={reset}></i>
-              <p id="searchResult" />
+              <div id="searchResult" />
             </form>
          </div>
-         <p id="description">Click icon to search</p>
       </div>
     </div>
   );
